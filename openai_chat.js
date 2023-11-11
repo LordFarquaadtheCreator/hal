c85f289ad02bfa2751ce4e59683c747a5d19ea3c
Fahad Faruqi <fahadfaruqi1@gmail.com>
2023-11-11 02:34:00 -0500

    fixed some stuff

--- openai_chat.js
+++ openai_chat.js
@@ -1,33 +1,27 @@
-// Import the necessary module from the 'openai' package
 const { OpenAI } = require('openai');
+const apiKey = process.env.OPENAI_API_KEY;
+require('dotenv').config();
 
-// Define a function to generate a response
+
 function generateChatResponse(apiKey, prompt) {
-  // Create an instance of the OpenAI class with your API key
   const openai = new OpenAI({
     apiKey: apiKey,
   });
-  // Send a request to the OpenAI API with the provided prompt
   return openai.chat.completions.create({
-    model: 'gpt-3.5-turbo', // Using the GPT-3.5 Turbo model (Can upgrade to GPT-4)
+    model: 'gpt-3.5-turbo',
     messages: [
       { role: 'system', content: 'You are a helpful assistant.' },
-      { role: 'user', content: prompt }, // User's prompt/question
+      { role: 'user', content: prompt }, 
     ],
   });
 }
-//OpenAI API key
-const apiKey = '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'; // Replace with your OpenAI API key
 
-//The main function to run the code
 async function main() {
   try {
-    //prompt
     const prompt = 'Can you provide me with a summary of the book "To Kill a Mockingbird"?';
-    // Calling the generateChatResponse function with the API key and prompt
+
     const response = await generateChatResponse(apiKey, prompt);
 
-    //Print the generated response from the OpenAI model
     console.log(response.choices[0].message.content);
     
     // Handle any errors that may occur
@@ -35,5 +29,4 @@ async function main() {
     console.error('Error:', error);
   }
 }
-
 main();
